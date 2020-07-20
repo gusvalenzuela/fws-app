c1324dbcff78d9657d8e8a82b1dea53f47e69091
Gus (Nala) <gusrosales7@gmail.com>
2020-07-19 18:40:37 -0700

    basic nextjs template

---
+++ src/components/Foot/index.js
@@ -0,0 +1,13 @@
+export default function Foot() {
+  return (
+    <footer className="footer">
+      <div style={{ display: "flex" }}>
+        <span> &copy; 2020 </span>
+        <a href="https://gusvalenzuela.github.io/gusval-site-bs/">
+          \\ Gus Valenzuela
+        </a>
+        <a href="https://github.com/gusvalenzuela/">\\ code</a>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/GameSchedule/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GameSchedule = () => {
+  return <table action="" id="table">
+    <tbody>
+
+    </tbody>
+  </table>;
+};
+
+export default GameSchedule;
