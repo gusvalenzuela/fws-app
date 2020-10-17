281e7902eca5f516c0558d4848655bfd361dc064
Gus (Nala) <gusrosales7@gmail.com>
2020-10-17 00:14:05 -0700

    Add hook to grab teams from DB. Save to store

--- lib/hooks.jsx
+++ lib/hooks.jsx
@@ -15,6 +15,11 @@ export function useSchedule(sport, season) {
   const schedule = data ? data.schedule : null;
   return [schedule];
 }
+export function useTeams(sport, season) {
+  const { data } = useSWR(`/api/teams/${sport}&${season}`, fetcher);
+  const teams = data ? data.teams : null;
+  return [teams];
+}
 
 export function useCurrentUser() {
   const { data, mutate } = useSWR("/api/user", fetcher);
