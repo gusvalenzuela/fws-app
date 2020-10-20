600814229847c6efac3944d387572a37529a1b97
Gus (Nala) <gusrosales7@gmail.com>
2020-10-20 11:23:42 -0700

    Winner is determined and saved to line_ property anytime the schedule is patched

--- pages/api/schedule/index.js
+++ pages/api/schedule/index.js
@@ -6,14 +6,33 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.patch(async (req, res) => {
-  // if (!req.user?.isAdmin) {
-  //   return res.status(401).send("unauthenticated");
-  // }
+  if (!req.user?.isAdmin) {
+    return res.status(401).send("unauthenticated");
+  }
   // req.body = JSON.parse(req.body); // body is Stringified on the PATCH request
   // console.log(req.body);
   let events = req.body;
   let results = [];
   await events.forEach(async (e) => {
+    // set a winner for the matchup adjusted with the point spread
+    if (e.event_status === "STATUS_FINAL" && !e.line_?.winner) {
+      var homeScore = e.home_score;
+      var awayScore = e.away_score;
+      var homeTeam = e.home_team_id;
+      var awayTeam = e.away_team_id;
+      if (e.line_?.favorite === homeTeam) {
+        // if the home team is the favorite
+        // add the point spread (negative num) to the away_team (underdog)
+        awayScore = awayScore - e.line_?.point_spread;
+      } else {
+        // the away team is the favorite
+        // add the point spread (negative num) to the home_team (underdog)
+        homeScore = homeScore - e.line_?.point_spread;
+      }
+      // determine who won
+      homeScore > awayScore ? (e.winner = homeTeam) : (e.winner = awayTeam);
+    }
+
     try {
       // query needs to find the correct sport and within it the correct event in its events Array
       const query = {
@@ -32,6 +51,7 @@ handler.patch(async (req, res) => {
           "events.$.home_score": e.home_score,
           "events.$.event_status": e.event_status,
           "events.$.event_status_detail": e.event_status_detail,
+          "events.$.line_.winner": e.winner || undefined,
         },
       };
       const updateOptions = {
