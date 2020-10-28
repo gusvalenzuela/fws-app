b52c9afdec41e93e341fa37a68b35d1ad9497a14
Gus (Nala) <gusrosales7@gmail.com>
2020-10-27 22:15:58 -0700

    Update to using aggregate in mongo for user's picks

--- pages/api/picks/index.js
+++ pages/api/picks/index.js
@@ -14,25 +14,25 @@ handler.get(async (req, res) => {
   res.status(200).json({ picks: picks });
 });
 
-handler.post(async (req, res) => {
-  if (!req.user) {
-    return res.status(401).send("unauthenticated");
-  }
+// handler.post(async (req, res) => {
+//   if (!req.user) {
+//     return res.status(401).send("unauthenticated");
+//   }
 
-  // const { selection } = req.body;
+//   // const { selection } = req.body;
 
-  // if (!selection) return res.status(400).send("You must write something");
+//   // if (!selection) return res.status(400).send("You must write something");
 
-  const pick = {
-    _id: nanoid(),
-    createdAt: new Date(),
-    userId: req.user._id,
-    ...req.body,
-  };
+//   const pick = {
+//     _id: nanoid(),
+//     createdAt: new Date(),
+//     userId: req.user._id,
+//     ...req.body,
+//   };
 
-  await req.db.collection("pickz").insertOne(pick);
-  return res.send(pick);
-});
+//   await req.db.collection("pickz").insertOne(pick);
+//   return res.send(pick);
+// });
 
 handler.patch(async (req, res) => {
   if (!req.user) {
