aae677fe3eba57df5271df4641aaf033fcdab33f
guzilla(NALA) <13578427+gusvalenzuela@users.noreply.github.com>
2021-01-16 15:43:19 -0800

    Changed role of toast container to 'dialog'

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,17 +3,17 @@ Global CSS cannot be imported from files other than your Custom <App>.
 Please move all global CSS imports to src\pages\_app.js.
 Read more: https://err.sh/next.js/css-global
 */
-import { useEffect } from "react";
-import Moment from "react-moment";
-import { ToastContainer } from "react-toastify";
-import Store from "../lib/stores/FootballPool";
-import Menubar from "../components/Menubar";
-import Footer from "../components/Footer";
+import React, { useEffect } from 'react'
+import Moment from 'react-moment'
+import { ToastContainer } from 'react-toastify'
+import Store from '../lib/stores/FootballPool'
+import Menubar from '../components/Menubar'
+import Footer from '../components/Footer'
 // import { Loader, Dimmer } from "semantic-ui-react";
-import { week_start_days as weekStartDates } from "../lib/stores/startDays.json";
-import "./_app.css";
-import "semantic-ui-css/semantic.min.css";
-import "react-toastify/dist/ReactToastify.css";
+import { week_start_days as weekStartDates } from '../lib/stores/startDays.json'
+import './_app.css'
+import 'semantic-ui-css/semantic.min.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 // This default export is required in a new `pages/_app.js\x` file.
 export default function MyApp({ Component, pageProps }) {
@@ -21,21 +21,22 @@ export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     // Start the pooled timer which runs every 1 second(s)
     // (60000 milliseconds) by default.
-    Moment.startPooledTimer(1000);
+    Moment.startPooledTimer(1000)
 
-    //startDateIndex finds the first date that is not in the past
+    // startDateIndex finds the first date that is not in the past
     // for the start of the nfl week (wednesday)
-    let startDateIndex = weekStartDates.findIndex(
+    const startDateIndex = weekStartDates.findIndex(
       (date) => Date.parse(date) > Date.now()
-    );
+    )
     // use that startDate to mark the week shown when the user first logs in
+    // if startDate is negative (i.e. out of bounds) use last week ([array].length == "weeks" in array)
     Store.setState({
       currentWeek:
         startDateIndex < 0 ? weekStartDates.length : startDateIndex + 1,
-      Moment: Moment,
-    });
+      Moment,
+    })
     // Store.setState({ Moment: Moment });
-  }, []);
+  }, [])
 
   return (
     <>
@@ -44,11 +45,11 @@ export default function MyApp({ Component, pageProps }) {
         // limit={3}
         newestOnTop
         // hideProgressBar
-        role="notification"
+        role="dialog"
         position="top-center"
       />
       <Component {...pageProps} />
       <Footer />
     </>
-  );
+  )
 }
