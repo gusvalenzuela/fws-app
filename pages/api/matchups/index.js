2ca518446feda49d7ba327c61bd4cdd87177c4eb
guzilla <gusrosales7@gmail.com>
2021-06-29 09:34:38 -0700

    Added endpoint for getting and patching matchups

---
+++ pages/api/matchups/index.js
@@ -0,0 +1,31 @@
+import nextConnect from 'next-connect'
+import middleware from '../../../middlewares/middleware'
+import { updateMatchups, getSchedule } from '../../../lib/db'
+
+const handler = nextConnect()
+
+handler.use(middleware)
+
+handler.get(async (req, res) => {
+  const { sport, yr } = req.query
+  if (!sport || !yr) return res.json(null)
+
+  return res.json(await getSchedule(req, sport, Number(yr)))
+})
+
+handler.patch(async (req, res) => {
+  const { user, body, db } = req
+  // if no Admin user
+  if (!user.isAdmin) {
+    return res.status(401).send('unauthenticated')
+  }
+  // check to confirm body is array
+  // ...
+  
+  const matchups = await updateMatchups(db, body)
+
+  return res.json({ matchups })
+})
+
+
+export default handler
