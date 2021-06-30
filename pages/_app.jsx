93a398b1a142d5bf75fc91fd46175283dcc4c9aa
guzilla <gusrosales7@gmail.com>
2021-06-29 17:35:22 -0700

    Removed unnecessary useCurrentUser hook

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,7 @@ import React, { useEffect } from 'react'
 import Moment from 'react-moment'
 import { ToastContainer } from 'react-toastify'
 import { week_start_days as weekStartDates } from '../lib/stores/startDays.json'
-import { useAllUsers, useCurrentUser } from '../lib/hooks'
+import { useAllUsers } from '../lib/hooks'
 import Store from '../lib/stores/FootballPool'
 import Menubar from '../components/Menubar'
 import Footer from '../components/Footer'
@@ -19,7 +19,7 @@ import 'react-toastify/dist/ReactToastify.css'
 // This default export is required in a new `pages/_app.js\x` file.
 export default function MyApp({ Component, pageProps }) {
   const [users] = useAllUsers()
-  const [currentUser] = useCurrentUser()
+
   // on mount
   useEffect(() => {
     // Start the pooled timer which runs every 1 second(s)
@@ -32,15 +32,15 @@ export default function MyApp({ Component, pageProps }) {
       (date) => Date.parse(date) > Date.now()
     )
     // use that startDate to mark the week shown when the user first logs in
-    // if startDate is negative (i.e. out of bounds) use last week ([array].length == "weeks" in array)
+    // if startDate is negative (i.e. out of bounds)
+    //  use last week ([array].length == "weeks" in array)
     Store.setState({
       currentWeek:
         startDateIndex < 0 ? weekStartDates.length : startDateIndex + 1,
       currentSeasonYear: 2020,
       Moment,
-      currentUser,
     })
-  }, [currentUser])
+  }, [])
 
   return (
     <>
