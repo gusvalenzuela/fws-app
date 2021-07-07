20707c66327ece31306db92eb3def302319829f0
guzilla <gusrosales7@gmail.com>
2021-07-07 09:35:18 -0700

    Remove (comment out) Sanity client

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,12 +1,13 @@
 import { MongoClient } from 'mongodb'
-import SC from '@sanity/client'
+// import SC from '@sanity/client'
 
-const sanityClient = SC({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  token: process.env.SANITY_TEAMS_TOKEN, // we need this to get write access
-  useCdn: false, // We can't use the CDN for writing
-})
+// const sanityClient = SC({
+//   projectId: process.env.SANITY_PROJECT_ID,
+//   dataset: process.env.SANITY_DATASET,
+//   token: process.env.SANITY_TEAMS_TOKEN, // we need this to get write access
+//   apiVersion: '2021-06-29',
+//   useCdn: false, // We can't use the CDN for writing
+// })
 
 const client = new MongoClient(process.env.MONGODB_URX, {
   useNewUrlParser: true,
@@ -33,7 +34,7 @@ export default async function database(req, res, next) {
   if (!client.isConnected()) await client.connect()
   req.dbClient = client
   req.db = client.db(process.env.DB_NAME)
-  req.SanityClient = sanityClient
+  // req.SanityClient = sanityClient
   // await setUpDb(req.db)
 
   return next()
