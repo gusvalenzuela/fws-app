f097e914fd0f9b6ef639ffb0d620d32cdef010de
guzilla <gusrosales7@gmail.com>
2021-07-07 15:15:44 -0700

    Rename matchups endpoint

--- pages/api/matchups/[sport]/[yr]/[week]/index.js
+++ pages/api/matchups/[sport]/[yr]/[week]/index.js
@@ -1,16 +1,16 @@
 import nextConnect from 'next-connect'
-import middleware from '../../../middlewares/middleware'
-import { updateMatchups, getSchedule } from '../../../lib/db'
+import middleware from '../../../../../../middlewares/middleware'
+import { updateMatchups, getSchedule } from '../../../../../../lib/db'
 
 const handler = nextConnect()
 
 handler.use(middleware)
 
 handler.get(async (req, res) => {
-  const { sport, yr } = req.query
+  const { sport, yr, week } = req.query
   if (!sport || !yr) return res.json(null)
 
-  return res.json(await getSchedule(req, sport, Number(yr)))
+  return res.json(await getSchedule(req, sport, Number(yr), Number(week)))
 })
 
 handler.patch(async (req, res) => {
@@ -21,11 +21,10 @@ handler.patch(async (req, res) => {
   }
   // check to confirm body is array
   // ...
-  
+
   const matchups = await updateMatchups(db, body)
 
   return res.json({ matchups })
 })
-
 
 export default handler
