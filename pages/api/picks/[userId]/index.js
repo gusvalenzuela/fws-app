392711829ad43989d96c67068fd5941747c3c210
guzilla <gusrosales7@gmail.com>
2021-07-12 10:51:40 -0700

    Add validation for 'week' param, as picks are retrieved by week now

--- pages/api/picks/[userId]/index.js
+++ pages/api/picks/[userId]/index.js
@@ -9,9 +9,12 @@ handler.use(middleware)
 handler.get(async (req, res) => {
   const { user } = req
   if (!user) return res.status(401).send('Please log in')
-  if (!req.query.userId) res.send(null)
 
-  const userPicks = await getUserPicks(req, req.query.userId)
+  const { userId, week } = req.query
+
+  if (!userId && !week) return res.json({ picks: null })
+
+  const userPicks = await getUserPicks(req, userId, week)
 
   return res.status(200).json({ picks: userPicks })
 })
