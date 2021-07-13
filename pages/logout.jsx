2ea6b0c4d00033f850eba6cb92130be6692ba920
guzilla <gusrosales7@gmail.com>
2021-07-13 10:09:02 -0700

    Fix logout pattern removing useEffect

--- pages/logout.jsx
+++ pages/logout.jsx
@@ -1,39 +1,35 @@
-import React, { useEffect } from "react";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useCurrentUser } from "../lib/hooks";
+import React from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useCurrentUser } from '../lib/hooks'
 
 const LogoutPage = () => {
-  const router = useRouter();
-  const [user, { mutate }] = useCurrentUser();
+  const router = useRouter()
+  const currentUser = useCurrentUser()
+  const { mutate } = currentUser[1]
 
-  useEffect(() => {
-    logOut();
-  }, []);
-
-  async function logOut() {
-    const res = await fetch("/api/auth", {
-      method: "DELETE",
-    });
+  fetch('/api/auth', {
+    method: 'DELETE',
+  }).then((res) => {
     if (res.status === 204) {
-      mutate({});
+      mutate({})
     }
-    router.push("/");
-  }
+    router.push('/')
+  })
 
   return (
     <main id="logout">
       <Head>
-        <title>FWS | Logged Out</title>
+        <title>FWS | Logout</title>
       </Head>
       <div className="main-content">
         <header className="page-header">Come back soon!</header>
         <div className="page-content">
-          <h2>You've been succesfully logged out</h2>
+          <h2>You are being logged out</h2>
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default LogoutPage;
+export default LogoutPage
