746b7b9cd2c73b817dfc2fd3c2ab0587b0838e57
guzilla <gusrosales7@gmail.com>
2021-07-28 11:44:44 -0700

    Fix issue with mongodb aggregate pipeline not being an array causing 500 error

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,13 +1,4 @@
 import { MongoClient } from 'mongodb'
-// import SC from '@sanity/client'
-
-// const sanityClient = SC({
-//   projectId: process.env.SANITY_PROJECT_ID,
-//   dataset: process.env.SANITY_DATASET,
-//   token: process.env.SANITY_TEAMS_TOKEN, // we need this to get write access
-//   apiVersion: '2021-06-29',
-//   useCdn: false, // We can't use the CDN for writing
-// })
 
 const client = new MongoClient(process.env.MONGODB_URX, {
   // useNewUrlParser: true,
@@ -30,11 +21,10 @@ export async function setUpDb(db) {
   )
 }
 
-export default async function database(req, res, next) {
-  if (!client.isConnected()) await client.connect()
+export default async function database(req, _res, next) {
+  await client.connect()
   req.dbClient = client
   req.db = client.db(process.env.DB_NAME)
-  // req.SanityClient = sanityClient
   // await setUpDb(req.db)
 
   return next()
