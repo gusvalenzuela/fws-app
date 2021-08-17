053b148d8cadb462345214f1fa4b3b14480c0c67
guzilla <gusrosales7@gmail.com>
2021-08-17 12:35:56 -0700

    Add exported types: SportsMatchup & SportsTeam. Extend next's GetServerSideProps interface to include custom requests

--- additional.d.ts
+++ additional.d.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest } from 'next'
+import type { NextApiRequest, GetServerSidePropsContext } from 'next'
 import type { MongoClient, Db } from 'mongodb'
 
 declare namespace JSX {
@@ -9,8 +9,14 @@ declare namespace JSX {
 export interface CustomNextApiRequest extends NextApiRequest {
   dbClient: MongoClient
   db: Db
+  user?: { _id: string | number }
 }
-export interface LeaderboardPlayer {
+export interface CustomGetServerSidePropsContext
+  extends GetServerSidePropsContext {
+  user?: { _id: string }
+  req: CustomNextApiRequest
+}
+export type LeaderboardPlayer = {
   seasonYear: number
   userId: string
   week: number
@@ -19,4 +25,36 @@ export interface LeaderboardPlayer {
   losses?: Array<string>
   user: { name: string; isAdmin?: boolean }
   winPercent?: number
+}
+export type SportsTeam = {
+  sportId: number
+  team_id: number
+  name: string
+  mascot: string
+  abbreviation: string
+  record?: string | [string]
+}
+export type SportsMatchup = {
+  sport_id: number
+  matchupId: string
+  event_id?: string
+  away_team_id: number
+  away_score?: number
+  broadcast?: string | Array<string>
+  date_event?: string | Date
+  event_date: string | Date
+  event_location: string
+  event_name?: string
+  event_status?: string // 'STATUS_SCHEDULED' | 'STATUS_FINAL'
+  event_status_detail?: string
+  home_team_id: number
+  home_score?: number
+  line_?: { point_spread: number; favorite: number }
+  season_type: 'Regular Season' | 'Postseason'
+  season_year: number
+  updatedAt: number | Date
+  week: number
+  week_detail?: string
+  week_name?: string
+  winner?: number
 }
