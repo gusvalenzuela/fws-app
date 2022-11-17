8526018146216a95c32acbb14147342b2f493964
gus valenzuela <gusrosales7@gmail.com>
2022-11-16 22:39:24 -0800

    Add proptypes, update year from 2021 to 2022, and add spacing

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,34 +1,48 @@
 import React from 'react'
-
-// Footer.propTypes = {
-//   darkMode: PropTypes.boolean.isRequired,
-// }
+import PropTypes from 'prop-types'
 
 const Footer = ({ darkMode }) => (
   <>
     <style jsx>{`
-      footer.footer {
+      footer.footer,
+      .footer-content {
         position: relative;
         display: flex;
+        justify-content: space-between;
+      }
+      footer.footer {
         // background: var(--color2, --main-white, #fff);
         border-top: 4px solid var(--brand-color1, red);
         margin-top: 5px;
         padding: 1rem 1rem 5rem;
-        justify-content: center;
         background-color: var(--main-${darkMode ? 'black' : 'white'});
         color: var(--main-${darkMode ? 'white' : 'black'});
       }
+      .footer-content {
+        margin: auto;
+        width: 100%;
+        max-width: 800px;
+      }
       .footer span {
         color: var(--main-${darkMode ? 'white' : 'black'});
-        margin-left: 5px;
+        // margin-left: 5px;
       }
     `}</style>
     <footer className="footer">
-      <span> &copy; 2020-2021 </span>
-      <a href="https://gusvalenzuela.com/"> \\ Gus Valenzuela</a>
-      <a href="https://github.com/gusvalenzuela/fws-app"> \\ code</a>
+      <div className="footer-content">
+        <span>
+          &copy; 2020-2022{' '}
+          <a href="https://gusvalenzuela.com/"> \\ Gus Valenzuela</a>
+        </span>
+
+        <a href="https://github.com/gusvalenzuela/fws-app">source code</a>
+      </div>
     </footer>
   </>
 )
 
 export default Footer
+
+Footer.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+}
