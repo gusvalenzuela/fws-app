ac3b4e0d4c6a085c0428c7b46a7b6150f56095de
gus valenzuela <gusrosales7@gmail.com>
2022-11-16 16:39:35 -0800

    Fix to only display 17 weeks if season year before 2021

--- components/WeekDropdown.tsx
+++ components/WeekDropdown.tsx
@@ -5,6 +5,9 @@ import Store from '../lib/stores/FootballPool'
 
 const WeekDropdown = () => {
   const week = Store((s) => s.week || s.currentWeek)
+  const year = Store((s) => s.seasonYear || s.currentSeasonYear)
+
+  const maxWeek = year < 2021 ? 17 : 18
 
   return (
     <>
@@ -12,7 +15,7 @@ const WeekDropdown = () => {
       <Dropdown
         className="custom-dropdown"
         onChange={(e, { value }) => Store.setState({ week: value })}
-        options={generateNumbersArray().map((num) => ({
+        options={generateNumbersArray(1, maxWeek).map((num) => ({
           key: num,
           value: num,
           text: `${num}`,
